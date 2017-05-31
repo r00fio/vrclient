a8bc14fdd5daa4f796b2c28fe947ce701b38a9ca
Yury Grybkov <r00fi0@Yurys-MacBook-Air.local>
2017-05-31 11:42:42 +0300

    all calculations carry out by device

--- www/js/controllers/device-orientation-controller.js
+++ www/js/controllers/device-orientation-controller.js
@@ -13,86 +13,120 @@ application.controller('DeviceOrientationCtrl', ['$scope', '$state', '$rootScope
             window.screen.lockOrientation('portrait');
         }
 
-        $scope.rotation = {};
-        $scope.old = {};
-        $scope.skipSendData = false;
+        $scope.calibrateSensors = function () {
+            $timeout(function () {
+                $scope.stop.tmpCounter.x = 0;
+                $scope.stop.x = $scope.rotation.accelerometer.x;
+                
+                $scope.stop.tmpCounter.z = 0;
+                $scope.stop.z = $scope.rotation.accelerometer.z;
+                
+                $rootScope.$applyAsync();
+                
+                var audio = new Audio('sounds/calibration_complete.mp3')
+                audio.volume = 1;
+                audio.play();
+            }, 2000)
+        };
+
+        $scope.offset = 4000;
         $scope.url = localStorage.getItem('host');
+        $scope.rotation = {accelerometer: {}};
+        $scope.stop = {
+            x: 3970,
+            y: 3970,
+            z: 3970,
+            tmpCounter: {
+                x: 0,
+                z: 0
+            },
+            countLimit: {
+                x: 100,
+                z: 100
+            }
+        };
+        $scope.direction = {
+            x: 'stop',
+            z: 'stop',
+            old: {
+                x: 'stop',
+                z: 'stop'
+            }
+        };
+        $scope.sensivity = {stop: {x: 100, z: 200}};
 
         $timeout(function () {
-            var watchID = cordova.plugins.magnetometer.watchReadings(onSuccess, onError)
-            // var options = {frequency: 7};  // Update every 3 seconds
+            // var watchID = cordova.plugins.magnetometer.watchReadings(onSuccess, onError);
+            var options = {frequency: 30};  // Update every 3 seconds
             navigator.accelerometer.watchAcceleration(success, onError, options);
         }, 3000);
-        
+
         function success(result) {
             result.ax = result.x;
             result.ay = result.y;
             result.az = result.z;
             onSuccess(result);
         }
-        
+
         function onSuccess(result) {
-            $scope.rotation.ax = Math.round(result.ax * 100);
-            $scope.rotation.ay = Math.round(result.ay * 100);
-            $scope.rotation.az = Math.round(result.az * 100);
-            
-            $scope.rotation.mx = Math.round(result.mx * 100);
-            $scope.rotation.my = Math.round(result.my * 100);
-            $scope.rotation.mz = Math.round(result.mz * 100);
-            
-            $scope.rotation.gx = Math.round(result.gx * 100);
-            $scope.rotation.gy = Math.round(result.gy * 100);
-            $scope.rotation.gz = Math.round(result.gz * 100);
-            $scope.rotation.timestamp = result.timestamp;
-            //
-            // $scope.rotation.x = result.quaternion.x * 100;
-            // $scope.rotation.y = result.quaternion.y * 100;
-            // $scope.rotation.z = result.quaternion.z * 100;
-            // $scope.rotation.mx = result.mx * 100;
-            // $scope.rotation.my = result.my * 100;
-            // $scope.rotation.mz = result.mz * 100;
-            // $scope.rotation.w = result.w * 100;
-            
-            // $scope.rotation.pitch = result.eulerAngles.pitch;
-            // $scope.rotation.roll = result.eulerAngles.roll;
-            // $scope.rotation.yaw = result.eulerAngles.yaw;
-            
+            $scope.rotation.accelerometer.x = Math.round(result.ax * 100) + $scope.offset;
+            $scope.rotation.accelerometer.y = Math.round(result.ay * 100) + $scope.offset;
+            $scope.rotation.accelerometer.z = Math.round(result.az * 100) + $scope.offset;
+
             $rootScope.$applyAsync();
-            sendData();
-            // console.log('success')
 
-        };
+            if ($scope.rotation.accelerometer.z > $scope.stop.z + $scope.sensivity.stop.z) {
+                $scope.direction.z = 'right'
+            } else if ($scope.rotation.accelerometer.z < $scope.stop.z - $scope.sensivity.stop.z) {
+                $scope.direction.z = 'left'
+            } else {
+                $scope.direction.z = 'stop';
+            }
 
+            if ($scope.rotation.accelerometer.x > $scope.stop.x + $scope.sensivity.stop.x) {
+                $scope.direction.x = 'up'
+            } else if ($scope.rotation.accelerometer.x < $scope.stop.x - $scope.sensivity.stop.x) {
+                $scope.direction.x = 'down'
+            } else {
+                $scope.direction.x = 'stop';
+            }
+
+            if ($scope.direction.x != $scope.direction.old.x ||
+                $scope.direction.z != $scope.direction.old.z) {
+                $scope.direction.old.z = $scope.direction.z;
+                $scope.direction.old.x = $scope.direction.x;
+                sendDirection();
+            }
+
+            //Auto-calibration
+            if ($scope.direction.x === 'stop') {
+                if (++$scope.stop.tmpCounter.x > $scope.stop.countLimit.x) {
+                    $scope.stop.tmpCounter.x = 0;
+                    $scope.stop.x = $scope.rotation.accelerometer.x;
+                }
+            } else {
+                $scope.stop.tmpCounter.x = 0;
+            }
+
+            if ($scope.direction.z === 'stop') {
+                if (++$scope.stop.tmpCounter.z > $scope.stop.countLimit.z) {
+                    $scope.stop.tmpCounter.z = 0;
+                    $scope.stop.z = $scope.rotation.accelerometer.z;
+                }
+            } else {
+                $scope.stop.tmpCounter.z = 0;
+            }
+        }
 
         function onError(fusionError) {
             $scope.magnetometerError = 'error';
         };
 
-
-        function sendData() {
-
-            var data = {
-                w: $scope.rotation.w,
-                ax: $scope.rotation.ax,
-                ay: $scope.rotation.ay,
-                az: $scope.rotation.az,
-                yaw:$scope.rotation.yaw,
-                pitch:$scope.rotation.pitch,
-                roll:$scope.rotation.roll,
-                mx: $scope.rotation.mx,
-                my: $scope.rotation.my,
-                mz: $scope.rotation.mz,
-                gx: $scope.rotation.gx,
-                gy: $scope.rotation.gy,
-                gz: $scope.rotation.gz,
-                
-                T: $scope.rotation.timestamp
-            }
-
+        function sendDirection() {
             $.ajax({
                 type: 'POST',
                 url: $scope.url + '/orientation',
-                data: data,
+                data: $scope.direction,
                 success: function (data) {
                 },
                 dataType: 'json',
--- www/js/controllers/start-controller.js
+++ www/js/controllers/start-controller.js
@@ -2,10 +2,17 @@ var application = angular.module('app');
 
 application.controller('StartCtrl', ['$scope', '$state', '$rootScope',
     function ($scope, $state, $rootScope) {
-        
-        $scope.host = {url: 'http://192.168.0.100:3000'};
-        $scope.$watch('host.url', function() {
-            localStorage.setItem('host', $scope.host.url);
+
+        $scope.host = {
+            url: 'http://192.168.43.223:3000',
+            port: 3000,
+            ip: '192.168.43.223'
+        };
+        $scope.$watch('host.ip', function () {
+            localStorage.setItem('host', 'http://' + $scope.host.ip + ':' + $scope.host.port);
+        });
+        $scope.$watch('host.port', function () {
+            localStorage.setItem('host', 'http://' + $scope.host.ip + ':' + $scope.host.port);
         });
 
         if (!localStorage.getItem('host')) {
